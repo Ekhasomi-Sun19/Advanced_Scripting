04a0830774ed1b498ca408a2eb64c11d08973e24
Ekhasomi-Sun19 <Ekh19001@byui.edu>
2023-05-02 19:10:44 -0600

    New Update

---
+++ Script/hello.ps1
@@ -0,0 +1,12 @@
+<#
+Program Name : hello.ps1
+Date: 05/02/2023
+Author: Sunday Ekhasomi
+Course: CIT361
+I, Sunday Ekhasomi, affirm that I wrote this script as original work completed by me.
+#>
+
+'Hello Wonderful world!'
+
+$name=Read-host "What is your name?"
+Write-Host "Good day $name. It is $(Get-Date)."
